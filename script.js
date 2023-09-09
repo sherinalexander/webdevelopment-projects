87f57b4d60f21c7d2c3893e4d66ba235bfc94d11
Sherin S <sherinsrilalexander2003.s@gmail.com>
2023-09-09 10:08:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+ let button=document.getElementById('add');
+ let todoList=document.getElementById('todolist');
+ let input=document.getElementById('input');
+//local storage
+
+ 
+ let todos = [];
+ window.onload = ()=>{
+  todos = JSON.parse(localStorage.getItem('todos'))||[]
+  todos.foreach(todo=>addtodo(todo))
+  
+ }
+
+ button.addEventListener('click',()=>{
+   todos.push(input.value)
+   localStorage.setItem('todos',JSON.stringify(todos))
+   addtodo(input.value)
+   input.value=''
+ })
+ function addtodo(todo){
+    let para = document.createElement('p')
+    para.innerText = todo;
+    todoList.appendChild(para)
+    
+    para.addEventListener('click',()=>{
+        para.style.textDecoration = 'line-through'
+        remove(todo)
+
+    })
+    para.addEventListener('dblclick',()=>{
+        todoList.removeChild(para)
+        remove(todo)
+
+    })
+ }
+
+    function remove(todo){
+        let index = todos.indexOf(todo)
+        if(index>-1){
+        todos.splice(index,1);
+        }
+    }
